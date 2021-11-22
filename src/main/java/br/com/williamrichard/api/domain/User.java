219229b04ec936dffa26c782f730bc6145728964
William Richard <wrichaard@hotmail.com>
2021-11-21 22:52:26 -0300

    :sparkles: added user class

---
+++ src/main/java/br/com/williamrichard/api/domain/User.java
@@ -0,0 +1,21 @@
+package br.com.williamrichard.api.domain;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+    private String password;
+}
