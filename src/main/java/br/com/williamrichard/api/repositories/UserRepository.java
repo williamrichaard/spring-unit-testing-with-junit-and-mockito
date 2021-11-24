9dbdc7e5e790990008d8ccd335f24d5574c65040
William Richard <wrichaard@hotmail.com>
2021-11-24 06:09:51 -0300

    :sparkles: added interface user repository

---
+++ src/main/java/br/com/williamrichard/api/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package br.com.williamrichard.api.repositories;
+
+import br.com.williamrichard.api.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+}
