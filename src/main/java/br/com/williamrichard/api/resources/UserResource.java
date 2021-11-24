fcf949a12487ec42736c1200e4c3ceee2cd93161
William Richard <wrichaard@hotmail.com>
2021-11-24 06:11:44 -0300

    :sparkles: added user class resource findById

--- src/main/java/br/com/williamrichard/api/resources/UserResource.java
+++ src/main/java/br/com/williamrichard/api/resources/UserResource.java
@@ -1,6 +1,8 @@
 package br.com.williamrichard.api.resources;
 
 import br.com.williamrichard.api.domain.User;
+import br.com.williamrichard.api.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,9 +13,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/user")
 public class UserResource {
 
+    @Autowired
+    private UserService service;
+
     @GetMapping(value = "/{id}")
     public ResponseEntity<User> findById(@PathVariable Integer id) {
-
-        return ResponseEntity.ok().body(new User(1, "William", "william@mail.com", "123"));
+        return ResponseEntity.ok().body(service.finById(id));
     }
 }
