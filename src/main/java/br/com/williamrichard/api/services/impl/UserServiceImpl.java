71fec8b307b85c3e6099435d9927ce244db86e53
William Richard <wrichaard@hotmail.com>
2021-11-24 06:10:50 -0300

    :sparkles: added user class service impl findById

---
+++ src/main/java/br/com/williamrichard/api/services/impl/UserServiceImpl.java
@@ -0,0 +1,22 @@
+package br.com.williamrichard.api.services.impl;
+
+import br.com.williamrichard.api.domain.User;
+import br.com.williamrichard.api.repositories.UserRepository;
+import br.com.williamrichard.api.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    @Override
+    public User finById(Integer id) {
+        Optional<User> obj = repository.findById(id);
+        return obj.orElse(null);
+    }
+}
