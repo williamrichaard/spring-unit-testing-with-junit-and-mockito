ec499f4b4658db7edada2da5a7fc905b893fe6c3
William Richard <wrichaard@hotmail.com>
2021-11-26 20:42:47 -0300

    :sparkles: returning dto to user resource

--- src/main/java/br/com/williamrichard/api/resources/UserResource.java
+++ src/main/java/br/com/williamrichard/api/resources/UserResource.java
@@ -1,7 +1,8 @@
 package br.com.williamrichard.api.resources;
 
-import br.com.williamrichard.api.domain.User;
+import br.com.williamrichard.api.domain.dto.UserDTO;
 import br.com.williamrichard.api.services.UserService;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,10 +15,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserResource {
 
     @Autowired
+    private ModelMapper mapper;
+
+    @Autowired
     private UserService service;
 
     @GetMapping(value = "/{id}")
-    public ResponseEntity<User> findById(@PathVariable Integer id) {
-        return ResponseEntity.ok().body(service.finById(id));
+    public ResponseEntity<UserDTO> findById(@PathVariable Integer id) {
+        return ResponseEntity.ok().body(mapper.map(service.finById(id), UserDTO.class));
     }
 }
