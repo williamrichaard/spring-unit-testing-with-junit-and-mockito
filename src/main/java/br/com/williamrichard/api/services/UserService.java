e2dd2c8d5ca2a995f580dd7687e6555b3bfe0848
William Richard <wrichaard@hotmail.com>
2021-11-26 21:03:39 -0300

    :sparkles: endpoint findAll for user

--- src/main/java/br/com/williamrichard/api/services/UserService.java
+++ src/main/java/br/com/williamrichard/api/services/UserService.java
@@ -2,7 +2,10 @@ package br.com.williamrichard.api.services;
 
 import br.com.williamrichard.api.domain.User;
 
+import java.util.List;
+
 public interface UserService {
 
     User finById(Integer id);
+    List<User> findAll();
 }
