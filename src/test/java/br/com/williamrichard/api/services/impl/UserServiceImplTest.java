cf0cd57feacefb7198a4342f9b34d17087d62719
William Richard <wrichaard@hotmail.com>
2021-11-29 23:11:23 -0300

    :test_tube: test for user service class method create successfully

--- src/test/java/br/com/williamrichard/api/services/impl/UserServiceImplTest.java
+++ src/test/java/br/com/williamrichard/api/services/impl/UserServiceImplTest.java
@@ -17,6 +17,7 @@ import java.util.Optional;
 
 import static org.hibernate.validator.internal.util.Contracts.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.when;
 
@@ -91,7 +92,17 @@ class UserServiceImplTest {
     }
 
     @Test
-    void create() {
+    void whenCreateThenReturnSuccess() {
+        when(repository.save(any())).thenReturn(user);
+
+        User response = service.create(userDTO);
+
+        assertNotNull(response);
+        assertEquals(User.class, response.getClass());
+        assertEquals(ID, response.getId());
+        assertEquals(NAME, response.getName());
+        assertEquals(EMAIL, response.getEmail());
+        assertEquals(PASSWORD, response.getPassword());
     }
 
     @Test
