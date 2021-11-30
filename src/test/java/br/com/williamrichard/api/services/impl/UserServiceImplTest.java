57177c81a7521a8f59c2d6cb02c72805f3aac4c8
William Richard <wrichaard@hotmail.com>
2021-11-29 23:32:20 -0300

    :test_tube: test for user service class create method with exception

--- src/test/java/br/com/williamrichard/api/services/impl/UserServiceImplTest.java
+++ src/test/java/br/com/williamrichard/api/services/impl/UserServiceImplTest.java
@@ -3,6 +3,7 @@ package br.com.williamrichard.api.services.impl;
 import br.com.williamrichard.api.domain.User;
 import br.com.williamrichard.api.domain.dto.UserDTO;
 import br.com.williamrichard.api.repositories.UserRepository;
+import br.com.williamrichard.api.services.exceptions.DataIntegratyViolationException;
 import br.com.williamrichard.api.services.exceptions.ObjectNotFoundException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -17,8 +18,7 @@ import java.util.Optional;
 
 import static org.hibernate.validator.internal.util.Contracts.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.when;
 
 @SpringBootTest
@@ -103,6 +103,19 @@ class UserServiceImplTest {
         assertEquals(NAME, response.getName());
         assertEquals(EMAIL, response.getEmail());
         assertEquals(PASSWORD, response.getPassword());
+    }
+
+    @Test
+    void whenCreateThenReturnAnDataIntegrityViolationException() {
+        when(repository.findByEmail(anyString())).thenReturn(optionalUser);
+
+        try{
+            optionalUser.get().setId(2);
+            service.create(userDTO);
+        } catch (Exception ex) {
+            assertEquals(DataIntegratyViolationException.class, ex.getClass());
+            assertEquals("E-mail já cadastrado no sistema", ex.getMessage());
+        }
     }
 
     @Test
