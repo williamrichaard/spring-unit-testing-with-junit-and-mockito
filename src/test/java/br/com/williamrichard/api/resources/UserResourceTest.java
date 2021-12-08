eff24c0c75db34951ba15bb8f6129ccd5c1c9396
William Richard <wrichaard@hotmail.com>
2021-12-07 23:31:10 -0300

    :beetle: removing bug

--- src/test/java/br/com/williamrichard/api/resources/UserResourceTest.java
+++ src/test/java/br/com/williamrichard/api/resources/UserResourceTest.java
@@ -31,8 +31,8 @@ class UserResourceTest {
     public static final String PASSWORD = "123";
     public static final int INDEX = 0;
 
-    private User user;
-    private UserDTO userDTO;
+    private User user = new User();
+    private UserDTO userDTO = new UserDTO();
 
     @InjectMocks
     private UserResource resource;
